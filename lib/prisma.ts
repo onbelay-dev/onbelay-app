b4943dfaafeea693c13a9ab17aac9b4cc1ef5895
Michael Schuler <mschuler15@gmail.com>
2026-01-06 13:36:43 -0500

    correcting Vercel error with prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,15 +2,13 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  // Allow global PrismaClient in dev to prevent multiple instances
-  // This avoids creating multiple connections during hot reloads
   var prisma: PrismaClient | undefined;
 }
 
 export const prisma =
   global.prisma ||
   new PrismaClient({
-    log: ["query"], // optional: logs SQL queries in console
+    log: ["query"],
   });
 
 if (process.env.NODE_ENV !== "production") global.prisma = prisma;
