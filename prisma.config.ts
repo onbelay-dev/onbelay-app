04724ef2b08e95e1f31456d4d1c0676a7faa07d7
Michael Schuler <mschuler15@gmail.com>
2026-01-15 12:23:58 -0500

    Revert "resolved vulnerabilities by updating prisma to 6.19.2 which caused an issue in Prisma Config ts file"
    This reverts commit 14eeda9576ac4ead8b293acf89f3b0668efb2a28.
    
    reverting vulnerability patches until further along

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"]!, // <- this MUST point to your Neon URL
+    url: process.env["DATABASE_URL"], // <- this MUST point to your Neon URL
   },
 });
